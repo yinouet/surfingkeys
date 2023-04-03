fdf44c91ef7479629c141cd6847016d1f00fc03a
yinouet <52395173+yinouet@users.noreply.github.com>
2023-04-03 10:13:47 +0900

    fix

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -1,7 +1,7 @@
 api.mapkey('ymd', "Copy current page's link with markdown", function() {
   const url = new URL(window.location.href);
   var title = window.document.title.replace(/(<|>)/g, '\\$1')
-  api.Clipboard.write(`[${title}](${url.href})`);
+  Clipboard.write(`[${title}](${url.href})`);
 });
 
 // an example to create a new mapping `ctrl-y`
