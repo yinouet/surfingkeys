01946533062a755ae8dce050e6609b207c81bbd9
yinouet <52395173+yinouet@users.noreply.github.com>
2023-04-06 15:00:59 +0900

    Update surfingkeys-config.js

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -1,3 +1,4 @@
+// Copy URL and Title in markdown link format
 api.mapkey('ymd', "Copy current page's link with markdown", function() {
   const url = new URL(window.location.href);
   var title = window.document.title.replace(/(<|>)/g, '\\$1')
@@ -5,15 +6,18 @@ api.mapkey('ymd', "Copy current page's link with markdown", function() {
 });
 
 // an example to create a new mapping `ctrl-y`
-api.mapkey('<ctrl-y>', 'Show me the money', function() {
-    Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
-});
+// api.mapkey('<ctrl-y>', 'Show me the money', function() {
+//     Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
+// });
 
 // an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
-api.map('gt', 'T');
+// api.map('gt', 'T');
+api.map('J', 'E');
+api.map('K', 'R');
+
 
 // an example to remove mapkey `Ctrl-i`
-api.unmap('<ctrl-i>');
+// api.unmap('<ctrl-i>');
 
 // set theme
 settings.theme = `
